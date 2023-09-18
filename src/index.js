eecb73c1d1295cbf2df0727fbbf50d393d38becc
Rioba Ian <riobafelix@gmail.com>
2023-09-18 13:04:36 +0300

    models

--- src/index.js
+++ src/index.js
@@ -1,6 +1,27 @@
+require("dotenv").config();
+
 const express = require("express");
+const mongoose = require("mongoose");
+const mongodbConnString = process.env.MONGODB_URI;
 
 const app = express();
+
+// parse json
+app.use(express.json());
+
+// mongodb connection
+
+mongoose.connect(mongodbConnString);
+
+const db = mongoose.connection;
+
+db.on("error", (err) => {
+  console.error(err);
+});
+
+db.once("connected", () => {
+  console.log(`Database connected`);
+});
 
 const apiRoutes = require("./routes/index");
 
---
+++ src/models/model.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const itemsSchema = new Schema({
+  name: { required: true, type: String, maxLength: 255 },
+  description: { type: String, required: true },
+  category: { type: Schema.Types.ObjectId, ref: "Category", required: true },
+  price: { type: Number },
+  in_stock: { type: Number },
+  images: [String],
+});
+
+itemsSchema.virtual("url").get(function () {
+  return `/shoppingitems/${this._id}`;
+});
+
+module.exports = mongoose.model("Items", itemsSchema);
